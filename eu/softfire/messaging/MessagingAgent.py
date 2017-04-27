e4bab701512299e14be7902b3e86e5ce4f35b658
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-04-27 18:16:23 +0200

    add dispatch of requests

--- eu/softfire/messaging/MessagingAgent.py
+++ eu/softfire/messaging/MessagingAgent.py
@@ -32,7 +32,8 @@ def register():
     stub = messages_pb2_grpc.RegistrationServiceStub(channel)
     response = stub.register(
         messages_pb2.RegisterMessage(name=config.get("system", "name"),
-                                     endpoint="%s:%s" % (config.get("system", "ip"), config.get("messaging", "bind_port")),
+                                     endpoint="%s:%s" % (
+                                     config.get("system", "ip"), config.get("messaging", "bind_port")),
                                      description=config.get("system", "description")))
     print("XXX manager received registration response: %s" % response.result)
 
@@ -44,10 +45,38 @@ def unregister():
     stub = messages_pb2_grpc.RegistrationServiceStub(channel)
     response = stub.unregister(
         messages_pb2.UnregisterMessage(name=config.get("system", "name"),
-                                       endpoint="%s:%s" % (config.get("system", "ip"), config.get("messaging", "bind_port"))))
+                                       endpoint="%s:%s" % (
+                                       config.get("system", "ip"), config.get("messaging", "bind_port"))))
     print("XXX manager received unregistration response: %s" % response.result)
 
 
 class ManagerAgent(messages_pb2_grpc.ManagerAgentServicer):
     def execute(self, request, context):
+        if request.method == messages_pb2.LIST_RESOURCES:
+            try:
+                return messages_pb2.ResponseMessage(result=0,
+                                                    list_resource=self.list_resources(payload=request.payload))
+            except Exception as e:
+                return messages_pb2.ResponseMessage(result=2, error_message=e)
+        if request.method == messages_pb2.PROVIDE_RESOURCES:
+            try:
+                return messages_pb2.ResponseMessage(result=0,
+                                                    provide_resource=self.provide_resources(payload=request.payload))
+            except Exception as e:
+                return messages_pb2.ResponseMessage(result=2, error_message=e)
+        if request.method == messages_pb2.RELEASE_RESOURCES:
+            try:
+                self.release_resources(payload=request.payload)
+                return messages_pb2.ResponseMessage(result=0)
+            except Exception as e:
+                return messages_pb2.ResponseMessage(result=2, error_message=e)
+
+    def list_resources(self, payload=None):
+        resources = [messages_pb2.ResourceMetadata("name", "descriprion", 1)]
+        return messages_pb2.ListResourceResponse(resources=resources)
+
+    def provide_resources(self, payload=None):
+        return messages_pb2.ProvideResourceResponse(resources="")
+
+    def release_resources(self, payload=None):
         pass
